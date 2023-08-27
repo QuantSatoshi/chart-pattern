ba0bb64c91400f09fd2eaf620c17ed9360e5bdae
wanghao <whateverhow@gmail.com>
2023-08-27 15:09:31 -0700

    [pattern]   (use "git restore --staged <file>..." to unstage)

--- src/utils/macdDivergence.ts
+++ src/utils/macdDivergence.ts
@@ -11,15 +11,14 @@ export function macdDivergence({
   priceWindow: SlidingWindowArr<number>;
 }) {
   // if price is rising, but macd has down trend, then it is diverged
-  if (priceWindow.length() === 0 || macdWindow.length() === 0)
-  {
+  if (priceWindow.length() === 0 || macdWindow.length() === 0) {
     throw new Error(`macdDivergence require non empty values`);
   }
   if (priceWindow.last()! > priceWindow.first()! && macdWindow.last()! < macdWindow.first()!) {
     return 1;
   }
   if (priceWindow.last()! < priceWindow.first()! && macdWindow.last()! > macdWindow.last()!) {
-    return -1; 
+    return -1;
   }
-  return 0; 
+  return 0;
 }

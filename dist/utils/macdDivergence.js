c8d7b0eb91392fb28b2693147c83784a025a5640
wanghao <whateverhow@gmail.com>
2023-08-27 15:13:16 -0700

    [pattern]   (use "git restore --staged <file>..." to unstage)

--- dist/utils/macdDivergence.js
+++ dist/utils/macdDivergence.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.macdDivergence = void 0;
 function macdDivergence({ macdWindow, priceWindow, macdSignal, }) {
     // if price is rising, but macd has down trend, then it is diverged
-    if (priceWindow.length() === 0 || macdWindow.length() === 0)
+    if (priceWindow.length() === 0 || macdWindow.length() === 0) {
         throw new Error(`macdDivergence require non empty values`);
+    }
     if (priceWindow.last() > priceWindow.first() && macdWindow.last() < macdWindow.first()) {
         return 1;
     }
